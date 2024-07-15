d357cae9f8f1cd0b8f3531d7dfa061f793dbafb2
Maureen E <maureenekeugo1@gmail.com>
2024-07-15 14:40:51 -0400

    Completed nav section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './Hero.css'
 
+
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div className='hero container'>
         <div className="hero-text">
-            <h1>r Allow us the privilege to capture every precious moment</h1>
+            {/* <h1>Allow us the privilege to capture every precious moment</h1> */}
+            <p>Step into a world of timeless love and unforgettable moments with our enchanting wedding photo gallery</p>
+            <button className='btn'>Explore</button>
         </div>
     </div>
   )
