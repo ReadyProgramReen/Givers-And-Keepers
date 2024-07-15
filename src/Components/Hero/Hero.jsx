9e9c3e34275d8bdc4e0c9da6237e5d61ae032bea
Maureen E <maureenekeugo1@gmail.com>
2024-07-15 13:35:39 -0400

    Added a new component

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className="hero-text">
+            <h1>r Allow us the privilege to capture every precious moment</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
