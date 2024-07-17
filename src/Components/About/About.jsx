c80d642c3dc6278c8c898876c2904a40ea24b950
Maureen E <maureenekeugo1@gmail.com>
2024-07-17 10:59:00 -0400

    Styled About component

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,10 +7,10 @@ const About = () => {
     <div className='about'>
         <div className="about-left">
             <h3>About Us</h3>
-            <h5>CLEAN, MODERN, TIMELES</h5>
+            <h5>CLEAN, MODERN, TIMELESS</h5>
             <hr />
-            <p>based in the heart of New York City, we specialize in 
-                capturing the essence of small, intimate weddings. Our mission is to artfully preserve the love and unique bond shared between two people on their special day. With a blend of creativity, professionalism, and a deep appreciation for love stories, we strive to deliver timeless photographs that beautifully narrate the beginning of your journey together. We are committed to providing a personalized experience that exceeds expectations, ensuring that every cherished moment 
+            <p>Based in the heart of New York City, we specialize in 
+                capturing the essence of intimate weddings. Our mission is to artfully preserve the love and unique bond shared between two people on their special day. With a blend of creativity, professionalism, and a deep appreciation for love stories, we strive to deliver timeless photographs that beautifully narrate the beginning of your journey together. We are committed to providing a personalized experience that exceeds expectations, ensuring that every cherished moment 
                 is elegantly documented to be treasured for a lifetime.</p>
 
         </div>
