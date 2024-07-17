6406f2ce899b07b8ab92a97fa8107b589d1b4ff7
Maureen E <maureenekeugo1@gmail.com>
2024-07-17 04:51:37 -0400

    Added an about component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
 import Price from './Components/Price/Price'
 import Title from './Components/Title/Title'
+import About from './Components/About/About'
 
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <div className="container">
+        <About/>
         <Title title ='Wedding Photgraphy Prices Packages'/>
         <Price/>
       </div>
---
+++ src/Components/About/About.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './About.css'
+import about_img from '../../assets/about1.png'
+
+const About = () => {
+  return (
+    <div className='about'>
+        <div className="about-left">
+            <h3>About Us</h3>
+            <h5>CLEAN, MODERN, TIMELES</h5>
+            <hr />
+            <p>based in the heart of New York City, we specialize in 
+                capturing the essence of small, intimate weddings. Our mission is to artfully preserve the love and unique bond shared between two people on their special day. With a blend of creativity, professionalism, and a deep appreciation for love stories, we strive to deliver timeless photographs that beautifully narrate the beginning of your journey together. We are committed to providing a personalized experience that exceeds expectations, ensuring that every cherished moment 
+                is elegantly documented to be treasured for a lifetime.</p>
+
+        </div>
+        <div className="about-right">
+            <img className='about-img' src={about_img} alt="" />
+        </div>
+
+
+    </div>
+  )
+}
+
+export default About
