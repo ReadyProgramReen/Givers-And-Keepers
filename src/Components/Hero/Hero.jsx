7aefed5c9e13d5db1dcacacaf1475bb12a528839
Maureen E <maureenekeugo1@gmail.com>
2024-07-18 10:42:22 -0400

    added media query for the hero component

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -6,9 +6,7 @@ const Hero = () => {
   return (
     <div className='hero container'>
         <div className="hero-text">
-            {/* <h1>Allow us the privilege to capture every precious moment</h1> */}
             <p>Step into a world of timeless love and unforgettable moments with our enchanting wedding photo gallery</p>
-            <button className='btn'>Explore</button>
         </div>
     </div>
   )
