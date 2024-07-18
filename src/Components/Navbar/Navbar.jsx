6c2c93337007eb0b57f19ae39d07e77dbda98e89
Maureen E <maureenekeugo1@gmail.com>
2024-07-18 10:22:16 -0400

    added media query for the nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
-import logo from '../../assets/logo.png'
+import menu_icon from '../../assets/menu-icon.png'
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
@@ -13,10 +13,16 @@ const Navbar = () => {
     })
   },[])
 
+  
+  const [mobileMenu, setMobileMenu] =useState(false)
+  const toggleMenu = ()=>{
+    mobileMenu? setMobileMenu(false):setMobileMenu(true)
+  }
+
   return (
     <nav className={`container ${sticky ? "dark-nav":""}`} >
         <p>Givers & Keepers</p>
-        <ul>
+        <ul className={mobileMenu?'':'hidden-menu'}>
             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
             <li><Link to='photos' smooth={true} offset={-150} duration={500}>Gallery</Link></li>
             <li><Link to='about' smooth={true} offset={-80} duration={500}>About us</Link></li>
@@ -28,6 +34,7 @@ const Navbar = () => {
 
 
         </ul>
+        <img className='menu-icon' src={menu_icon} alt="" onClick={toggleMenu} />
     </nav>
   )
 }
