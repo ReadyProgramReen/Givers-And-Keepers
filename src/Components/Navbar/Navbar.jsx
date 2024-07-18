0abbf45bb661cbf2638575995037a25c6e7e2f12
Maureen E <maureenekeugo1@gmail.com>
2024-07-17 21:50:57 -0400

    Added scroll feature

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
 
@@ -16,12 +17,12 @@ const Navbar = () => {
     <nav className={`container ${sticky ? "dark-nav":""}`} >
         <p>Givers & Keepers</p>
         <ul>
-            <li>Home</li>
-            <li>Gallery</li>
-            <li>About us</li>
-            <li>Past Clients</li>
-            <li>Pricing</li>
-            <li><button className='btn'>Contact us</button></li>
+            <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
+            <li><Link to='photos' smooth={true} offset={-150} duration={500}>Gallery</Link></li>
+            <li><Link to='about' smooth={true} offset={-80} duration={500}>About us</Link></li>
+            <li><Link to='testimonials' smooth={true} offset={-190} duration={500}>Past Clients</Link></li>
+            <li><Link to='prices' smooth={true} offset={-165} duration={500}>Pricing</Link></li>
+            <li><Link to='contact' smooth={true} offset={-170} duration={500} className='btn'>Contact us</Link></li>
 
 
 
